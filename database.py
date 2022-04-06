4c410efa53e105ed8d8efe604c7bdf7d58c3f476
Israr_dawar <77443384+israr96418@users.noreply.github.com>
2022-04-06 12:57:19 +0500

    Update database.py

--- database.py
+++ database.py
@@ -11,7 +11,23 @@ def get_db():
 
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQL_ALCHEMY_DATABASE_URL = "mysql+mysqldb://isrardawar:dawar96418@localhost:3306/uetm"
+
+sqlalchemy_database_url = f"mysql+mysqldb://{setting.database_username}:{setting.database_password}@{setting.database_hostname}:{setting.database_portNumber}/{setting.database_name}"
+
+
+# create engine are responsible to connect fastapi with your database through url
+
+engine = create_engine(sqlalchemy_database_url)
+
+# sessionmaker are responsible to make conversation/communication with the database
+# the sessionmaker can provide a factory for Session objects
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# this ftn are responsible to convert all the model into table in databass
+# when we run the server again and again  it all time model will be converted into tables
+# this function also prevent to create the table again and again
+Base = declarative_base()
+
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
